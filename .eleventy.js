a1d9177bb9d0fe4394450afa251a6230a6da2773
Keir Ansell <hello@keiransell.com>
2024-02-27 02:12:43 +0000

    Work on the main structure, navigation and header.

--- .eleventy.js
+++ .eleventy.js
@@ -3,7 +3,7 @@ const eleventySass = require("@11tyrocks/eleventy-plugin-sass-lightningcss")
 module.exports = function(eleventyConfig) {
     
     eleventyConfig.addPlugin(eleventySass)
-
+    eleventyConfig.addPassthroughCopy('src/-')
     return {
         dir: {
             input: 'src',

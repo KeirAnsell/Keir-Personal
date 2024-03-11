8c12605e0ed14b3dfb97d5b93c8ba535015b4d79
Cory Dransfeldt <coryd@hey.com>
2024-03-11 10:58:59 -0700

    fix: masonry grid; style updates et al

--- .eleventy.js
+++ .eleventy.js
@@ -1,13 +1,18 @@
 const eleventySass = require("@11tyrocks/eleventy-plugin-sass-lightningcss")
 
 module.exports = function(eleventyConfig) {
-    
     eleventyConfig.addPlugin(eleventySass)
     eleventyConfig.addPassthroughCopy('src/-')
+    eleventyConfig.addPassthroughCopy({
+      'node_modules/masonry-simple/dist/masonry-simple.min.js': '-/scripts/masonry-simple.min.js'
+    })
     return {
-        dir: {
-            input: 'src',
-            output: 'public'
-        }
+      passthroughFileCopy: true,
+      dir: {
+        input: 'src',
+        includes: '_includes',
+        data: '_data',
+        output: 'public',
+      },
     }
 }
